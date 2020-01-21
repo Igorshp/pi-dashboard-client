a70c75e9022b254f8f0870c76245e7557cb8003b
Igor Shpakov <igor@sliide.com>
2020-01-21 14:50:25 +0000

    fix update script

--- update_and_run.sh
+++ update_and_run.sh
@@ -3,7 +3,7 @@
 # this script pulls latest version from git and runs client.py
 # it's launched with systemd on the pi to make upgrades easy
 
-git reset hard
+git reset --hard
 git pull origin/master
 
 pip3 install -r requirements.txt
